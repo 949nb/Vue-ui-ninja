b2c3d5c2c91d56fe5dd111a3742367914ade7b0c
hy949 <1661346771@qq.com>
2019-04-15 19:32:27 +0800

    添加菊花状态

--- src/button.vue
+++ src/button.vue
@@ -1,5 +1,6 @@
 <template>
   <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+    <g-icon class="loading" name="loading"></g-icon>
     <g-icon class="icon" v-if="icon"  :name="icon"></g-icon>
     <div class="content">
       <slot></slot>
@@ -24,6 +25,10 @@
 </script>
 
 <style lang="scss">
+  @keyframes spin {
+    0%{ transform: rotate(0deg)}
+    100%{ transform: rotate(360deg)}
+  }
   .g-button {
     font-size: var(--font-size);
     height: var(--button-height);
@@ -69,6 +74,9 @@
       > .content {
         order: 1;
       }
+    }
+    .loading{
+      animation: spin 1s infinite linear;
     }
   }
 </style>
