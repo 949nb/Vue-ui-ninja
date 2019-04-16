7f6d96d4502b46888a07377899a6f50683087476
hy949 <1661346771@qq.com>
2019-04-16 22:48:12 +0800

    第一个元素不需要 margin left -1px

--- src/button-group.vue
+++ src/button-group.vue
@@ -5,35 +5,36 @@
 </template>
 
 <script>
-export default{
-  mounted() {
-    console.log("here");
-    for(let node of this.$el.children){
-      let name = node.nodeName.toLowerCase()
-      if(name !== 'button'){
-        console.warn(`g-button-group 的子元素应该是 button,而你的是${name}`);
+  export default {
+    mounted() {
+      console.log("here");
+      for (let node of this.$el.children) {
+        let name = node.nodeName.toLowerCase();
+        if (name !== 'button') {
+          console.warn(`g-button-group 的子元素应该是 button,而你的是${name}`);
+        }
       }
     }
   }
-}
 </script>
 
 <style lang="scss">
-  .g-button-group{
-    display:flex;
+  .g-button-group {
+    display: flex;
     margin-top: 100px;
-    > .g-button{
+
+    > .g-button {
       border-radius: 0;
-      margin-left: -1px;
-      &:first-child{
+      &:not(:first-child){margin-left: -1px;}
+      &:first-child {
         border-top-left-radius: var(--button-radius);
         border-bottom-left-radius: var(--button-radius);
       }
-      &:last-child{
+      &:last-child {
         border-top-right-radius: var(--button-radius);
         border-bottom-right-radius: var(--button-radius);
       }
-      &:hover{
+      &:hover {
         position: relative;
         z-index: 1;
       }
