467f52aa916c2ea063068a1798c7b8f70a4586ed
hy949 <1661346771@qq.com>
2019-04-16 15:40:41 +0800

    添加 上一页 下一页

---
+++ src/button-group.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="g-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default{}
+</script>
+
+<style lang="scss">
+  .g-button-group{
+    display:flex;
+    margin-top: 100px;
+    > .g-button{
+      border-radius: 0;
+      &:first-child{
+        border-top-left-radius: var(--button-radius);
+        border-bottom-left-radius: var(--button-radius);
+      }
+      &:last-child{
+        border-top-right-radius: var(--button-radius);
+        border-bottom-right-radius: var(--button-radius);
+      }
+    }
+  }
+</style>
--- src/button.vue
+++ src/button.vue
@@ -34,14 +34,8 @@
     100%{ transform: rotate(360deg)}
   }
   .g-button {font-size: var(--font-size);height: var(--button-height);padding: 0 .9em; /*按钮的宽度*/
-    border-radius: var(--button-radius);
-    border: 1px solid var(--border-color);
-    background: var(--button-bg);
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    vertical-align: middle;
-    cursor: pointer;
+    border-radius: var(--button-radius);border: 1px solid var(--border-color);background: var(--button-bg);
+    display: inline-flex;justify-content: center;align-items: center;vertical-align: middle;cursor: pointer;
     &:hover {border-color: var(--border-color-hover);}
     &:active {background-color: var(--button-active-bg);}
     &:focus {outline: none;}
